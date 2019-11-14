ced8807ae6fbeeffffe4dab3cd8010581e0c5472
Jackson Jewell <jackson.t.jewell@gmail.com>
2019-11-14 16:04:01 -0600

    paranoia commit

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,15 @@
 let quizDiv = document.getElementById("quiz");
 let resultsDiv = document.getElementById("results");
+let currentTime = 75;
+let timeText = document.getElementById("pageTimer");
+var quizHide = document.getElementById("quiz"); 
+var scoreHide = document.getElementById("highScores"); 
+var highHide = document.getElementById("high"); 
+var start = document.getElementById("start");
+let page = 0;
+let scoreNum;
+quizHide.style.display = "none";
+scoreHide.style.display = "none";
 
 let questions = [
     {
@@ -15,48 +25,44 @@ let questions = [
     ///etc.
   ];
 
+function buildQuiz(){
+  //builds the quiz
+  //trigger timer
+  timer();
+  //build the questions
+  questions.forEach(questionMaker);
+  //hide the start button
+  start.style.display = "none";
+  //hide the high scores button
+  highHide.style.display = "none";
+  //show the quiz questions
+  quizHide.style.display = "block";
+}
+
+function timer(){
+  //starts the timer
   let currentTime = 75;
-  let timeText = document.getElementById("pageTimer");
-
-  
-  function timer(){
-    let currentTime = 75;
-    let myTime = setInterval(changeTime,1000);
-    return myTime;
-    }
+  let myTime = setInterval(changeTime,1000);
+  return myTime;
+  }
   
 
-  function changeTime(){
+function changeTime(){
+  //updates the timer text
     if(currentTime>-1){
       timeText.textContent = ("Time = "+currentTime);
       currentTime = currentTime - 1;
     }else{
       clearInterval(myTime);
     }
-    }
+  }
 
-    var quizHide = document.getElementById("quiz");
-
-    quizHide.style.display = "none";
-
-    function buildQuiz(){
-      timer();
-      questions.forEach(questionMaker);
-      hider = 1;
-      var start = document.getElementById("start");
-      if (start.style.display === "none") {
-        start.style.display = "block";
-      } else {
-        start.style.display = "none";
-      }
-      quizHide.style.display = "block";
-    }
-
-let page = 0;
     
-    
+
+
 
 function questionMaker(){
+  //builds the question on the screen
     for(var i = 0; i < 4;){
       let qLabels = document.querySelectorAll("input");
       let qAnswers = document.querySelectorAll("p");
@@ -70,3 +76,14 @@ function questionMaker(){
     }
   }
 
+  function score(){
+    scoreNum = currentTime;
+  }
+
+  function saveScore(){
+    //brings up a form that allows the user to input their name, then stores it on the scoreboard
+  }
+
+  function showScores(){
+    //brings up the high scores page
+  }
