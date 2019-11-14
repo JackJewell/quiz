7d65963936960c6d87ad9f5c355b4f29a5a0286b
Jackson Jewell <jackson.t.jewell@gmail.com>
2019-11-14 10:58:29 -0600

    timer created

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,4 @@
-var questions = [
+let questions = [
     {
       title: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
@@ -12,3 +12,24 @@ var questions = [
     ///etc.
   ];
   
+  function quiz(){
+    timer();
+  }
+
+  let currentTime= 75;
+  let timeText = document.getElementById("pageTimer");
+
+  
+  function timer(){
+    let myTime = setInterval(changeTime,1000);
+    }
+  
+
+  function changeTime(){
+    if(currentTime>-1){
+      timeText.textContent = ("Time = "+currentTime);
+      currentTime = currentTime - 1;
+    }else{
+      clearInterval(myTime);
+    }
+    }
